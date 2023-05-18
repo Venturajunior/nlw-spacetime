40b52b8e69d5bcf4fa56ce910829bc74d3c428cd
Ventura Junior <you@example.com>
2023-05-18 02:53:21 -0300

    Create web application base

---
+++ web/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="h-screen bg-zinc-800 p-6 text-zinc-100">
+      <h1 className="text-4xl font-bold">Hello world</h1>
+    </div>
+  )
+}
